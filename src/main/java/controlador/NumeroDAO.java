b6969ff676cca0de72265e269e66307a255195d0
TomasJesusVega <rlp23409@educastur.es>
2025-11-05 20:38:45 +0000

    nuevo commit

---
+++ src/main/java/controlador/NumeroDAO.java
@@ -0,0 +1,21 @@
+package controlador;
+
+public class NumeroDAO {
+
+	/**
+	 * while(ResultSet.next) {
+	 * Espectaculo esp = new Espectaculo();
+	 * esp.setID(rs.getLong("id");
+	 * esp.setNombre(rs.getString("nombre");
+	 * 
+	 * numeroDAO numDAO = new numDAO();
+	 * ArrayList<Numero> numeroEsp = numDAO.numeroDelEspectaculo(esp.getId());
+	 * esp.setNumeros(numerosEsp);
+	 * 
+	 * listaEsp.add(esp);
+	 * }
+	 * 
+	 * return 
+	 */
+	
+}
