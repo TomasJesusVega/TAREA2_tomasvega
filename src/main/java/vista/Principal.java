479749f75ad142836754eb72110cbc24f121093b
TomasJesusVega <rlp23409@educastur.es>
2025-11-13 20:19:52 +0000

    ssasad

--- src/main/java/vista/Principal.java
+++ src/main/java/vista/Principal.java
@@ -1,7 +1,17 @@
 package vista;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Principal {
 	public static void main(String[] args) {
-		
+		Connection conexion;
+		String jdbc = "jdbc:mysql://localhost:3306/circo_tomasvega";
+		try {
+			conexion = DriverManager.getConnection(jdbc, "usuario", "contrasenia");
+		} catch (SQLException sql) {
+			
+		}
 	}
 }
